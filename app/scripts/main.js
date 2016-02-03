9250f390aef42666c648f1977c3e49a5afd4e59c
Thomas Duval <thomas.duval@opmbx.org>
2016-02-03 08:48:19 +0100

    Add calculator function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,72 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var calculation = [];
+
+function one() {
+    calculation.push(1);
+}
+
+function two() {
+    calculation.push(2);
+}
+
+function three() {
+    calculation.push(3);
+}
+
+function four() {
+    calculation.push(4);
+}
+
+function five() {
+    calculation.push(5);
+}
+
+function six() {
+    calculation.push(6);
+}
+
+function seven() {
+    calculation.push(7);
+}
+
+function eight() {
+    calculation.push(8);
+}
+
+function nine() {
+    calculation.push(9);
+}
+
+function plus() {
+    calculation.push("+");
+}
+
+function minus() {
+    calculation.push("-");
+}
+
+function multiply() {
+    calculation.push("*");
+}
+
+function divide() {
+    calculation.push("/");
+}
+
+function firstBracket() {
+    calculation.push("(");
+}
+
+function lastBracket() {
+    calculation.push(")");
+}
+
+function clear() {
+    calculation = [];
+}
+
+function cancel() {
+    calculation.pop();
+}
+
+$(document).ready(function () {
+});
