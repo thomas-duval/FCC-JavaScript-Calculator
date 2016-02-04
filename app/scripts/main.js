442315448223da0464982de8f1709db002c747c8
Thomas Duval <thomas.duval@opmbx.org>
2016-02-04 08:17:32 +0100

    Add JS function for all numbers & operators

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,8 +13,64 @@ $(document).ready(function () {
         calculation.push(1);
         display()     
     });
+    $('#two').click(function () {
+        calculation.push(2);
+        display()     
+    });
+    $('#three').click(function () {
+        calculation.push(3);
+        display()     
+    });    
+    $('#four').click(function () {
+        calculation.push(4);
+        display()     
+    });    
+    $('#five').click(function () {
+        calculation.push(5);
+        display()     
+    });    
+    $('#six').click(function () {
+        calculation.push(6);
+        display()     
+    });    
+    $('#seven').click(function () {
+        calculation.push(7);
+        display()     
+    });    
+    $('#eight').click(function () {
+        calculation.push(8);
+        display()     
+    });    
+    $('#nine').click(function () {
+        calculation.push(9);
+        display()     
+    });    
+    $('#AC').click(function () {
+        calculation = [];
+        display()     
+    });    
+    $('#CE').click(function () {
+        calculation.pop();
+        display()     
+    });    
+    $('#percentage').click(function () {
+        calculation.push("/100");
+        display()     
+    });    
     $('#plus').click(function () {
         calculation.push("+");
+        display()     
+    });    
+    $('#minus').click(function () {
+        calculation.push("-");
+        display()     
+    });    
+    $('#multiply').click(function () {
+        calculation.push("*");
+        display()     
+    });    
+    $('#divide').click(function () {
+        calculation.push("/");
         display()     
     });
     $('#equal').click(function () {
